8719ce8f180ce4cf288384f0c7d1e4a37105fe23
Sjors Provoost <sjors@sprovoost.nl>
2016-07-08 23:06:32 +0100

    Use value.call()() instead of send()

--- contract.sol
+++ contract.sol
@@ -17,13 +17,6 @@ contract DaoChallenge
 	// Owner of the challenge; a real DAO doesn't an owner.
 	address owner;
 
-	function sendOrThrow(address destination, uint256 amount) {
-		bool result = destination.send(amount);
-		if (!result) {
-			throw;
-		}
-	}
-
 	function DaoChallenge () {
 		owner = msg.sender; // Owner of the challenge. Don't use this in a real DAO.
 	}
@@ -40,12 +33,20 @@ contract DaoChallenge
 		notifySellToken(amount, sender);
 	}
 
+	// This uses call.value()() rather than send(), but only sends to msg.sender
+	function withdrawEtherOrThrow(uint256 amount) {
+		bool result = msg.sender.call.value(amount)();
+		if (!result) {
+			throw;
+		}
+	}
+
 	function refund() noEther {
 		address sender = msg.sender;
 		uint256 tokenBalance = tokenBalanceOf[sender];
 		if (tokenBalance == 0) { throw; }
 		tokenBalanceOf[sender] = 0;
-		sendOrThrow(sender, tokenBalance * tokenPrice);
+		withdrawEtherOrThrow(tokenBalance * tokenPrice);
 		notifyRefundToken(tokenBalance, sender);
 	}
 
@@ -56,7 +57,7 @@ contract DaoChallenge
 		if (tokenBalanceOf[recipient] + tokens < tokenBalanceOf[recipient]) throw; // Check for overflows
 		tokenBalanceOf[sender] -= tokens;
 		tokenBalanceOf[recipient] += tokens;
-		notifyTranferToken(tokens, sender, recipient);       
+		notifyTranferToken(tokens, sender, recipient);
 	}
 
 	// The owner of the challenge can terminate it. Don't use this in a real DAO.
