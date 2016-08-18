a4fe21a71588b33aaa2d5534d9da86dc5b79bf57
Sjors Provoost <sjors@sprovoost.nl>
2016-08-18 15:37:55 +0200

    feat(issueTokens): issue N tokens for X szabo before deadline

--- dao-challenge.sol
+++ dao-challenge.sol
@@ -6,13 +6,13 @@ contract DaoChallenge
 					Constants
 	***************************/
 
-	uint256 constant public tokenPrice = 1000000000000000; // 1 finney
 
 	/**************************
 					Events
 	***************************/
 
 	event notifyTerminate(uint256 finalBalance);
+	event notifyTokenIssued(uint256 n, uint256 price, uint deadline);
 
 	event notifyNewAccount(address owner, address account);
 	event notifyBuyToken(address owner, uint256 tokens, uint256 price);
@@ -22,6 +22,12 @@ contract DaoChallenge
 	/**************************
 	     Public variables
 	***************************/
+
+	// For the current token issue:
+	uint public tokenIssueDeadline = now;
+	uint256 public tokensIssued = 0;
+	uint256 public tokensToIssue = 0;
+	uint256 public tokenPrice = 1000000000000000; // 1 finney
 
 	mapping (address => DaoAccount) public daoAccounts;
 
@@ -91,9 +97,40 @@ contract DaoChallenge
 		return account.getTokenBalance();
 	}
 
+	// n: max number of tokens to be issued
+	// price: in szabo, e.g. 1 finney = 1,000 szabo = 0.001 ether
+	// deadline: unix timestamp in seconds
+	function issueTokens (uint256 n, uint256 price, uint deadline) noEther onlyChallengeOwner {
+		// Only allow one issuing at a time:
+		if (now < tokenIssueDeadline) throw;
+
+		// Deadline can't be in the past:
+		if (deadline < now) throw;
+
+		// Issue at least 1 token
+		if (n == 0) throw;
+
+		tokenPrice = price * 1000000000000;
+		tokenIssueDeadline = deadline;
+		tokensToIssue = n;
+		tokensIssued = 0;
+
+		notifyTokenIssued(n, price, deadline);
+	}
+
 	function buyTokens () returns (uint256 tokens) {
+		tokens = msg.value / tokenPrice;
+
+		if (now > tokenIssueDeadline) throw;
+		if (tokensIssued >= tokensToIssue) throw;
+
+		// This hopefully prevents issuing too many tokens
+		// if there's a race condition:
+		tokensIssued += tokens;
+		if (tokensIssued > tokensToIssue) throw;
+
 	  DaoAccount account = accountFor(msg.sender, true);
-		tokens = account.buyTokens.value(msg.value)();
+		if (account.buyTokens.value(msg.value)() != tokens) throw;
 
 		notifyBuyToken(msg.sender, tokens, msg.value);
 		return tokens;
